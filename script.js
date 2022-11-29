e839a43b62401233e07c3cdec674a3eb48c1e83f
scottrdee <109056605+scottrdee@users.noreply.github.com>
2022-11-28 20:37:03 -0800

    update css html js to complete project

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+
+    var year = new Date().getFullYear()+parseInt(years);
+
+    var result = document.getElementById('result');
+
+    if (principal <= 0) {
+        document.getElementById("principal").placeholder = "Enter a positive number";
+    } else {
+        result.innerHTML = 'If you deposit <em>' + principal +'</em><br>at an interest rate of <em>' + rate + '%</em>.<br>You will receive an amount of <em>' + interest + '</em>,<br>in the year <em>' + year + '</em>';
+    }
+
     
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
